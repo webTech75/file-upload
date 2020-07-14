e31044f484d054ccd5e72d76cbc1c63d2f88960f
Amr Elhabbal <webTech75@users.noreply.github.com>
2020-07-14 16:14:21 -0500

    Create Octogon.js

---
+++ file-upload/src/Octogon.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Octogon = () => (
+  <div className='overlay'>
+    <div className='uploading'>
+      <div className="octogon">
+        <div className="octo-sec top" />
+        <div className="octo-sec right" />
+        <div className="octo-sec bottom" />
+        <div className="octo-sec left" />
+      </div>
+    </div>
+  </div>
+);
+export default Octogon;
